b70b825d97fe661f3154b46b70affd250b1b79c0
Amir Mingaliev <amir.mingaliev@flatstack.com>
2016-09-06 17:27:53 +0300

    Fetch activities with ajax request before render template

--- app.js
+++ app.js
@@ -5,13 +5,24 @@
       'app.activated':'showFitnessActivity'
     },
 
+    requests: {
+      fetchActivities: function(settings, ticket) {
+        return {
+          url: settings.host + '/activities',
+          type: 'GET',
+          dataType: 'json',
+          headers: { 'X-Auth-Token': settings.pear_up_api_token },
+          data: { 'zendesk_id': ticket.requester().id() }
+        }
+      }
+    },
+
     showFitnessActivity: function() {
-      this.switchTo('fitness_activity',
-        { activities:
-          [
-            {date: "1.09.2016", steps: 5000},
-            {date: "2.09.2016", steps: 4500}
-          ]
+      this.ajax('fetchActivities', this.settings, this.ticket()).done(function(data) {
+        this.switchTo('fitness_activity', { activities: data });
+      }).fail(function(data){
+        error = data.responseJSON['rails_api_format/error'].error
+        this.switchTo('error_message', { error: error });
       });
     }
   };
