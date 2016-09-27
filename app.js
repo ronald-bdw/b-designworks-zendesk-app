5801e7494f77f7714c8f859371b36bd93acfcbb0
Amir Mingaliev <amir.mingaliev@flatstack.com>
2016-09-27 19:39:47 +0300

    Handle user changes events

--- app.js
+++ app.js
@@ -3,40 +3,75 @@
   return {
     events: {
       'app.activated': function() {
-        this.showFitnessActivity('day');
+        this.showFitnessActivity({ period: 'day', count: 7 });
       },
+
       'click .show-by-hour': function(event) {
-        this.showFitnessActivity('hour', this.$(event.target).text());
+        this.showFitnessActivity({ period: 'hour', date: this.$(event.target).text() });
       },
+
       'click .show-by-day': function() {
-        this.showFitnessActivity('day');
+        this.showFitnessActivity({ period: 'day', count: 7 });
+      },
+
+      '*.changed': function(e) {
+        this.updateUser(e.propertyName.split('.'), e.newValue);
       }
     },
 
     requests: {
-      fetchActivities: function(settings, ticket, period, date) {
+      fetchActivities: function(ticket, period, date, count) {
         return {
-          url: settings.host + '/activities',
+          url: this.setting('host') + '/activities',
           type: 'GET',
           dataType: 'json',
-          headers: { 'X-Auth-Token': settings.pear_up_api_token },
-          data: { 'zendesk_id': ticket.requester().id(), period: period, date: date, count: 7 }
+          headers: { 'X-Auth-Token': this.setting('pear_up_api_token') },
+          data: { 'zendesk_id': ticket.requester().id(), period: period, date: date, count: count }
+        };
+      },
+
+      updateUser: function(params) {
+        return {
+          url: this.setting('host') + '/zendesk/users/' + params.userId,
+          type: 'PATCH',
+          dataType: 'json',
+          headers: { 'X-Auth-Token': this.setting('pear_up_api_token') },
+          data: params.data
         };
       }
     },
 
-    showFitnessActivity: function(period, date) {
-      this.ajax('fetchActivities', this.settings, this.ticket(), period, date).done(function(data) {
+    updateUser: function([model, property], value) {
+      if(model !== 'user' && !this._.contains(["name", "email"], property)) return;
+
+      var params = { userId: this.user().id(), data: { user: {} } };
+      params.data.user[property] = value;
+
+
+      this.ajax('updateUser', params).done(function(){
+        var message = "The user's " + property + " was successfully synchronized."
+
+        this.switchTo("success_message", { message: message })
+      }).fail(function(data){
+        var message = "Can't synchronize user's " + property + ". Please check its value."
+
+        this.switchTo("error_message", { message: this.parseError(data, property) })
+      });
+    },
+
+    showFitnessActivity: function(params) {
+      if(this.ticket === undefined) return;
+
+      this.ajax('fetchActivities', this.ticket(), params.period, params.date, params.count).done(function(data) {
         var self = this;
 
         var activities = _.map(data, function(activity){
-          return { date: self.formatDate(activity.date, period), steps_count: activity.steps_count };
+          return { date: self.formatDate(activity.date, params.period), steps_count: activity.steps_count };
         });
 
-        this.switchTo('fitness_activity_by_' + period, { activities: activities });
+        this.switchTo('fitness_activity_by_' + params.period, { activities: activities });
       }).fail(function(data){
-        var error = data.responseJSON['rails_api_format/error'].error;
-        this.switchTo('error_message', { error: error });
+        this.switchTo('error_message', { message: "Something went wrong!" });
       });
     },
 
@@ -48,6 +83,14 @@
       } else {
         return moment_date.format("LL");
       }
+    },
+
+    parseError: function(data, property) {
+      var baseMessage = "Can't synchronize user's " + property + ". "
+
+      var addMessage = { name: "Please use client's first and last name" }[property]
+
+      return baseMessage + addMessage;
     }
   };
 }());
